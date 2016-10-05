66f825ec3a941c4673c299ce1eb5833d529d562b
Sebastian Döll <sebastian.doell@axelspringer.de>
2016-10-05 14:05:27 +0200

    chore(api) ~ some wip on the mock server

--- api/mock.js
+++ api/mock.js
@@ -1,25 +1,55 @@
 'use strict';
 
-const osprey    = require('osprey-mock-service');
 const express   = require('express');
+const fs        = require('fs');
+const osprey    = require('osprey-mock-service');
 const parser    = require('raml-parser');
 const path      = require('path');
+const shrinkRay = require('shrink-ray');
+const spdy      = require('spdy');
+// const https     = require('https'); // save for later
+const logger    = require('morgan');
 
 const app = express();
+app.use(shrinkRay());
+app.use(logger('dev'));
 
 const root  = __dirname;
 const api   = 'api.raml';
-const port  = '8080';
+const port  = '8443';
 
+const config = {
+  key: fs.readFileSync(path.join(root, 'server.key')),
+  cert: fs.readFileSync(path.join(root, 'server.crt'))
+};
+
+app.get('', (req, res) => { // default on root
+  res
+    .status(200)
+    .json({message: 'ok'});
+});
 
 parser.loadFile(path.join(root, api))
   .then(raml => {
+
     app.use(osprey.createServer(raml));
     app.use(osprey(raml));
-    app.listen(port);
+
+    spdy.createServer(config, app)
+      .listen(port, err => {
+        if (err) {
+          throw new Error(err);
+        } else {
+          console.log(`Listening on port: https://localhost:${port}`);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
   })
   .catch(err => {
 
-    console.error(new Error(err));
+    throw new Error(err);
 
   });
