0c49fecdf1cd2c14ace3e327308037955fcd4ea2
Sebastian Döll <sebastian.doell@axelspringer.de>
2016-11-02 09:00:11 +0100

    ~ fixing layout, removing compose and changing config for env

--- src/client/components/app/app.component.ts
+++ src/client/components/app/app.component.ts
@@ -1,7 +1,4 @@
 // Importables
-import { Angular2Apollo } from 'angular2-apollo';
-// import { ApolloQueryObservable } from 'angular2-apollo';
-// import { ApolloQueryResult } from 'apollo-client';
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
@@ -12,16 +9,12 @@ import { ViewEncapsulation } from '@angular/core';
 // Compponents
 import { AppConfig } from '../../config';
 import { EventEmitProvider } from '../../core';
-import { NotifyProvider } from '../../core';
-// import { NotifyEvent } from '../../core';
 import { DatabaseProvider } from './../../core';
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'sg-app',
-  styleUrls: [
-    './app.style.scss',
-  ],
+  styleUrls: ['./app.component.scss'],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
@@ -29,25 +22,19 @@ export class AppComponent implements OnInit {
   @ViewChild('menu') public menu;
 
   constructor(
-    private apollo: Angular2Apollo,
     private translate: TranslateService,
     private title: Title,
-    private notify: NotifyProvider,
   ) {
-
     translate.setDefaultLang(AppConfig.DEFAULT_LANGUAGE);
     translate.use(AppConfig.DEFAULT_LANGUAGE);
 
     EventEmitProvider
       .connect(DatabaseProvider.name)
       .subscribe(val => console.log(val));
-
   }
 
   public ngOnInit() {
-
     this.title.setTitle(AppConfig.HTML5_TITLE);
-
   }
 
 }
--- src/client/config/apollo.config.ts
+++ src/client/config/apollo.config.ts
@@ -1,6 +1,9 @@
 // Application Configuration
 export class ApolloConfig {
 
-  public static uri: string = 'https://localhost:8443/graphql'; // TODO@sebastian should be dynamic
+  public static uri: string =
+    ENV === 'development'
+      ? 'http://localhost:8080/graphql'
+      : 'http://editor.test.tortuga.cloud'; // TODO@sebastian should be dynamic
 
 };
