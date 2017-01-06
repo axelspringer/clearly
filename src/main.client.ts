0350858843b550869c04264258eb910d0277dd4a
Sebastian Döll <sebastian.doell@axelspringer.de>
2017-01-06 11:36:47 +0100

    ~ changing bootloader process

--- src/main.client.ts
+++ src/main.client.ts
@@ -2,6 +2,7 @@
  * Angular bootstraping
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootloader } from './bootloader';
 
 // for later, with Web Worker
 // import { platformWorkerAppDynamic } from '@angular/platform-webworker-dynamic';
@@ -18,4 +19,9 @@ import { AppModule } from './client';
 /*
  * Bootstrap our Angular app with a top level NgModule
  */
-platformBrowserDynamic().bootstrapModule(AppModule);
+export function main() {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+}
+
+// use bootloader in case of async tag
+bootloader(main);
